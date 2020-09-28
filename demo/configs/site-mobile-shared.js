e5c2bce5d25e7ef99321bd5b1af404cf30d78605
liuziyang <liuziyang@youzan.com>
2020-09-29 02:18:36 +0800

    fix: 添加demo

---
+++ demo/configs/site-mobile-shared.js
@@ -0,0 +1,86 @@
+
+  export const demos = {
+  
+};
+  export const config = {
+  "title": "Docx",
+  "defaultLang": "zh-CN",
+  "logo": {
+    "image": "https://img.yzcdn.cn/vant/logo.png",
+    "href": "#/"
+  },
+  "locales": {
+    "zh-CN": {
+      "text": "中文",
+      "nav": [
+        {
+          "name": "开发指南",
+          "items": [
+            {
+              "title": "快速上手",
+              "path": "quickstart"
+            },
+            {
+              "title": "更新日志",
+              "path": "changelog"
+            },
+            {
+              "title": "如何参与",
+              "path": "contribute"
+            },
+            {
+              "title": "文档规范",
+              "path": "markdown"
+            },
+            {
+              "title": "国际化",
+              "path": "locale"
+            }
+          ]
+        },
+        {
+          "name": "基础组件",
+          "items": [
+            {
+              "title": "Button 按钮",
+              "path": "button"
+            },
+            {
+              "title": "Cell 单元格",
+              "path": "cell"
+            },
+            {
+              "title": "Field 输入框",
+              "path": "field"
+            },
+            {
+              "title": "Icon 图标",
+              "path": "icons"
+            },
+            {
+              "title": "Navbar 导航栏",
+              "path": "navbar"
+            },
+            {
+              "title": "Popup 弹出层",
+              "path": "popup"
+            },
+            {
+              "title": "Rate 评分",
+              "path": "rate"
+            },
+            {
+              "title": "Search 搜索",
+              "path": "search"
+            },
+            {
+              "title": "Tag 标记",
+              "path": "tag"
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+  
